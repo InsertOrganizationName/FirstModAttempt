51ed432b00a4afd1eceebed511cd0ede8ada91d9
kirypto <kirypto@gmail.com>
2017-11-28 12:49:58 -0600

    Modified the TestBlock to duplicate itself when right-clicked

--- src/main/java/examplemod/TestBlock.java
+++ src/main/java/examplemod/TestBlock.java
@@ -1,13 +1,15 @@
 package examplemod;
 
 import net.minecraft.block.Block;
-import net.minecraft.block.BlockContainer;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.tileentity.TileEntity;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
-
-import javax.annotation.Nullable;
 
 public class TestBlock extends Block {
 
@@ -17,9 +19,20 @@ public class TestBlock extends Block {
         System.out.println("~~~~~~~~~~~~~~~~  Hit me!");
     }
 
-//    @Nullable
-//    @Override
-//    public TileEntity createNewTileEntity(World worldIn, int meta) {
-//        return new TestBlockEntity();
-//    }
+    @Override
+    public boolean onBlockActivated(World worldIn, BlockPos pos, IBlockState state, EntityPlayer playerIn, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
+        System.out.println("Clicked!");
+        Vec3d playerLookVector = playerIn.getLookVec();
+
+        BlockPos blockToReplace = new BlockPos(pos.getX() + Math.round(playerLookVector.x), pos.getY() + Math.round(playerLookVector.y), pos.getZ() + Math.round(playerLookVector.z));
+
+        boolean isBlockToReplaceAir = worldIn.getBlockState(blockToReplace).getBlock().getUnlocalizedName().equals("tile.air");
+
+        if (isBlockToReplaceAir) {
+            worldIn.setBlockState(blockToReplace, state);
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
