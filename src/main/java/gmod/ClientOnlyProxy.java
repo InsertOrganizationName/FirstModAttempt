6d03eabf9f09a0079d35c83e965d19a0b4a0166f
kirypto <kirypto@gmail.com>
2018-02-08 13:19:24 -0600

    Added keyboard capturing ability to test particle spawning.

--- src/main/java/gmod/ClientOnlyProxy.java
+++ src/main/java/gmod/ClientOnlyProxy.java
@@ -1,11 +1,21 @@
 package gmod;
 
+import gmod.particle.TextureStitcherBreathFx;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.entity.EntityPlayerSP;
+import net.minecraft.client.settings.KeyBinding;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.client.registry.ClientRegistry;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ClientOnlyProxy extends CommonProxy {
+
+    public static List<KeyBinding> keyBindings;
+
     @Override
     public boolean playerIsInCreativeMode(EntityPlayer player) {
         if (player instanceof EntityPlayerMP) {
@@ -20,5 +30,17 @@ public class ClientOnlyProxy extends CommonProxy {
     @Override
     public boolean isDedicatedServer() {
         return false;
+    }
+
+    @Override
+    public void preInit() {
+        super.preInit();
+
+        MinecraftForge.EVENT_BUS.register(new TextureStitcherBreathFx());
+
+        keyBindings = new ArrayList<>();
+        keyBindings.add(new KeyBinding("gmod.test", org.lwjgl.input.Keyboard.KEY_P, "key.gmod.category"));
+
+        keyBindings.forEach(ClientRegistry::registerKeyBinding);
     }
 }
--- src/main/java/gmod/particle/TextureStitcherBreathFx.java
+++ src/main/java/gmod/particle/TextureStitcherBreathFx.java
@@ -1,13 +1,42 @@
 package gmod.particle;
 
+import gmod.ClientOnlyProxy;
+import gmod.utils.Position;
+import gmod.utils.Velocity;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.entity.EntityPlayerSP;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.BlockPos;
 import net.minecraftforge.client.event.TextureStitchEvent;
+import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.InputEvent.KeyInputEvent;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class TextureStitcherBreathFx {
     @SubscribeEvent
     public void stitcherEventPre(TextureStitchEvent.Pre event) {
         ResourceLocation flameRL = new ResourceLocation("gmod:entity/flame_fx");
         event.getMap().registerSprite(flameRL);
+    }
+
+    @SideOnly(Side.CLIENT)
+    @SubscribeEvent(priority = EventPriority.NORMAL, receiveCanceled = true)
+    public void onEvent(KeyInputEvent keyInputEvent) {
+        ClientOnlyProxy.keyBindings.forEach(keyBinding -> {
+            if (keyBinding.isPressed()) {
+                System.out.println(String.format("Key Binding Pressed: %s", keyBinding.getKeyDescription()));
+
+                EntityPlayerSP player = Minecraft.getMinecraft().player;
+                BlockPos playerPosition = player.getPosition();
+
+                Minecraft.getMinecraft().effectRenderer.addEffect(
+                        FlameParticle.of(player.world,
+                                Position.of(playerPosition.getX(), playerPosition.getY() + 1, playerPosition.getZ()),
+                                Velocity.of(player.getLookVec().x, player.getLookVec().y, player.getLookVec().z))
+                );
+            }
+        });
     }
 }
