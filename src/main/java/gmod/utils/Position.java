7cf3c21040097b1b1301c7b87914afa7e51845f0
kirypto <kirypto@gmail.com>
2018-02-08 18:12:34 -0600

    Added static constructors to Position and Velocity that take in Vec3d

--- src/main/java/gmod/utils/Position.java
+++ src/main/java/gmod/utils/Position.java
@@ -1,9 +1,15 @@
 package gmod.utils;
 
+import net.minecraft.util.math.Vec3d;
+
 public class Position extends Vector3 {
 
     public static Position of(double x, double y, double z) {
         return new Position(x, y, z);
+    }
+
+    public static Position of(Vec3d vec3d) {
+        return new Position(vec3d.x, vec3d.y, vec3d.z);
     }
 
     private Position(double x, double y, double z) {
