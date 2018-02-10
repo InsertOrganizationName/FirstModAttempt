9f9ec1b2715fd8ad97e2f0af79ecb9631401454e
kirypto <kirypto@gmail.com>
2018-02-09 19:34:12 -0600

    Modified the KeyBindingHandler to fire LineParticles at all iron ingots within range

--- src/main/java/gmod/KeyBindingHandler.java
+++ src/main/java/gmod/KeyBindingHandler.java
@@ -1,16 +1,22 @@
 package gmod;
 
-import gmod.ClientOnlyProxy;
 import gmod.particle.FlameParticle;
+import gmod.particle.LineParticle;
 import insertorgname.anvil.utils.Position;
 import insertorgname.anvil.utils.Velocity;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.entity.EntityPlayerSP;
+import net.minecraft.entity.item.EntityItem;
+import net.minecraft.init.Items;
+import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.InputEvent.KeyInputEvent;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class KeyBindingHandler {
 
@@ -29,6 +35,27 @@ public class KeyBindingHandler {
                                 Position.of(player.getPositionEyes(0)),
                                 Velocity.of(player.getLookVec()))
                 );
+
+                List<EntityItem> entitiesWithinAABB = player.getEntityWorld().getEntitiesWithinAABB(EntityItem.class, new AxisAlignedBB(
+                        player.getPosition().getX() - 5, player.getPosition().getY() - 5, player.getPosition().getZ() - 5,
+                        player.getPosition().getX() + 5, player.getPosition().getY() + 5, player.getPosition().getZ() + 5
+                ));
+
+
+                System.out.println(entitiesWithinAABB.size());
+
+
+                List<EntityItem> ironIngotItems = entitiesWithinAABB.stream()
+                        .filter(entity -> entity.getItem().getItem().getRegistryName() == Items.IRON_INGOT.getRegistryName())
+                        .collect(Collectors.toList());
+
+                ironIngotItems.forEach(entityItem -> {
+                    Minecraft.getMinecraft().effectRenderer.addEffect(
+                            LineParticle.of(player.world,
+                                    player,
+                                    entityItem)
+                    );
+                });
             }
         });
     }
